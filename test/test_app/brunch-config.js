680c067eb026e803ec40b3a4594f1da7363727d0
Paul Medynski <31868385+paulmedynski@users.noreply.github.com>
2018-11-10 11:21:19 -0400

    Add a test app #4
    - Added a test app that exercises tslint-brunch with a real brunch build.

---
+++ test/test_app/brunch-config.js
@@ -0,0 +1,70 @@
+// Our test app's brunch config, based on brunch init.
+module.exports =
+{
+  // Disable OS notifications, since brunch will crash if you don't have the
+  // correct OS notifier subsystem installed.
+  notifications: false,
+
+  files:
+  {
+    // Build any JavaScript and TypeScript files in the default app/ dir.
+    javascripts: {joinTo: {'app.js': /\.[jt]s$/}},
+    // Package any styles as well.
+    stylesheets: {joinTo: 'app.css'}
+  },
+  
+  plugins:
+  {
+    // Let's use tslint to double-check that our TypeScript files don't have
+    // any problems.
+    //
+    // This config is duplicated in app/app-tslint.json for the command-line
+    // tslint tool to use.
+    tslint:
+    {
+      config:
+      {
+	extends: 'tslint:recommended',
+	rules:
+	{
+          'no-console': false,
+          'quotemark': false,
+          'one-line': false,
+          'no-empty': false,
+          'member-ordering': false
+	}
+      }
+    },
+
+    // Configure the typescript-brunch plugin.  These are compiler options
+    // that tsc would expect to find in a tsconfig.json file in the
+    // "compilerOptons" section.
+    //
+    // This config is duplicated in app/app-tsconfig.json for the command-line
+    // tslint tool to use.
+    brunchTypescript:
+    {
+      target: 'ES2015',
+      strict: true,
+      pretty: true,
+      sourceMap: true,
+      traceResolution: false
+    }
+  },
+
+  overrides:
+  {
+    production:
+    {
+      plugins:
+      {
+        off:
+        [
+          // Some of the app's brunch plugins don't work with really old
+          // versions of typescript, so we disable them here.
+          'uglify-js-brunch'
+        ]
+      }
+    }
+  }
+};
